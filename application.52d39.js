d0800311bfe149cb378640e91474e69779935eab
DESKTOP-RR6M8F2\PC <tientien187@gmail.com>
2026-04-27 20:54:04 +0700

    Update data

--- application.52d39.js
+++ application.52d39.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.a7251.json';
+          this.settingsPath = 'src/settings.feffa.json';
           this.showFPS = false;
         }
         init(engine) {
