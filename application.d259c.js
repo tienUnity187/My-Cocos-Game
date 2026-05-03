cb44e7bda1e1f947587b91f73896b26de30e4ed1
DESKTOP-RR6M8F2\PC <tientien187@gmail.com>
2026-05-03 21:27:47 +0700

    Update data

--- application.d259c.js
+++ application.d259c.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.1bc9e.json';
+          this.settingsPath = 'src/settings.8aee8.json';
           this.showFPS = false;
         }
         init(engine) {
--- index.e18c9.js
+++ index.e18c9.js
@@ -1,4 +1,4 @@
-System.register(["./application.820aa.js"], function (_export, _context) {
+System.register(["./application.d259c.js"], function (_export, _context) {
   "use strict";
 
   var Application, application;
